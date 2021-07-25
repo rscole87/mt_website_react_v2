e337e396cf0baec15023bb7332e989a5a09176bc
Rodrick Coleman <rscole87@gmail.com>
2021-07-24 20:42:05 -0400

    add projects section

--- src/components/FeaturedComponent.js
+++ src/components/FeaturedComponent.js
@@ -1,18 +1,25 @@
 import React from "react";
 import { Container, Row, Col } from "reactstrap";
+import PROJECTS from "../shared/projects";
 
 const Featured = () => {
+  const projectThumbs = PROJECTS.map((project) => {
+    return (
+      <div className="project-thumb">
+        <img src={project.image} alt={project.title} />
+      </div>
+    );
+  });
+
   return (
     <section id="featured-projects-section" className="d-flex align-items-center">
-      <Container>
-        <Row>
-          <Col sm="10">
+      <Container fluid>
+        <Row className="pb-5">
+          <Col className="text-center">
             <h3>Featured Projects</h3>
           </Col>
-          <Col sm="2">
-            <button>See all work</button>
-          </Col>
         </Row>
+        <Row className="justify-content-around">{projectThumbs}</Row>
       </Container>
     </section>
   );
---
+++ src/shared/projects.js
@@ -0,0 +1,61 @@
+import artsyOlive from "../images/project-artsy-olive.jpg";
+import magazineCovers from "../images/project-magazine-covers.jpg";
+import moscatoWine from "../images/project-moscato.jpg";
+import ssPrints from "../images/project-ss-prints.jpg";
+import woodMill from "../images/project-woodmill-cabinetry.jpg";
+import yogaLauren from "../images/project-yoga.jpg";
+import project501Cio from "../images/project-501cio.jpg"
+
+const PROJECTS = [
+  {
+    id: 0,
+    title: "Artsy Olive",
+    description: "",
+    image: artsyOlive,
+    type: ["branding"]
+  },
+  {
+    id: 1,
+    title: "Magazine Covers",
+    description: "",
+    image: magazineCovers,
+    type: ["layout"]
+  },
+  {
+    id: 2,
+    title: "Moscato Wine Bar & Tasting",
+    description: "",
+    image: moscatoWine,
+    type: ["branding"]
+  },
+  {
+    id: 3,
+    title: "SS Prints-Plus",
+    description: "",
+    image: ssPrints,
+    type: ["web-design", "web-development"]
+  },
+  {
+    id: 4,
+    title: "WoodMill Cabinetry",
+    description: "",
+    image: woodMill,
+    type: ["branding"]
+  },
+  {
+    id: 5,
+    title: "Yoga with Lauren Director",
+    description: "",
+    image: yogaLauren,
+    type: ["branding", "layout"]
+  },
+  {
+    id: 6,
+    title: "501Cio",
+    description: "",
+    image: project501Cio,
+    type: ["web-design"]
+  },
+];
+
+export default PROJECTS;
