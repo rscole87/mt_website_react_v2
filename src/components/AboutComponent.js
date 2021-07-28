4ac366cddf4a55ea842df7c69ca9a084e562db7c
Rodrick Coleman <rscole87@gmail.com>
2021-07-27 21:00:26 -0400

    resize pull quote for tablet, capitalize footer links, resize contact form for tablet

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,10 +9,10 @@ const About = () => {
           <Col sm="6" className="pt-4 pt-sm-0 pe-sm-5">
             <p className="body-copy"><strong>Marble Theory Design</strong> is a branding and design firm that helps companies put their best foot forward through refined design and stimulating visual creations. These creations build a unique brand experience for our clients which can be felt in all touch points.</p>
             <p>With nearly 10 years of industry experience, our team is well equipped to assist you with your creative needs and to help take your business to the next level.</p>
-            <span className="accent-text">The look, the feel, the experience.</span>
+            {/* <span className="accent-text">The look, the feel, the experience.</span> */}
           </Col>
 
-          <Col sm="6" className="pt-3 pt-sm-0 ps-sm-5" id="right-col">
+          <Col sm="6" className="pt-3 pt-sm-0 ps-sm-5 ps-md-3 d-flex align-items-center" id="right-col">
             <h3 className="pull-quote">Collaborative Solutions to Creative Problems</h3>
           </Col>
         </Row>
